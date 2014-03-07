94249097ec87890400d24c26560b8dd11620349f
Douvi <edouard.roussillon@gmail.com>
2014-03-06 17:34:32 -0800

    import project to git

---
+++ PhoneApp9Patch/PhoneApp9Patch/UI9Patch/UI/Image9Patch.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+
+namespace PhoneApp9Patch.UI9Patch.UI
+{
+    public partial class Image9Patch : UserControl
+    {
+
+        private BitmapImage imageSource;
+
+        public Image9Patch()
+        {
+            InitializeComponent();
+        }
+
+        private String _source;
+        public String Source
+        {
+            get { return _source; }
+            set
+            {
+                _source = value;
+
+                imageSource = new BitmapImage(new Uri(_source, UriKind.Relative));
+                imageSource.CreateOptions = BitmapCreateOptions.None;
+                imageSource.ImageFailed += new EventHandler<ExceptionRoutedEventArgs>(bi_ImageFailed);
+                imageSource.ImageOpened += new EventHandler<RoutedEventArgs>(bi_ImageOpened);
+            }
+        }
+
+        void bi_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            throw e.ErrorException;
+        }
+
+        void bi_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            NinePatch mNinePath = new NinePatch((BitmapImage)sender, (int)this.ActualWidth, (int)this.ActualHeight);
+            Image.Source = mNinePath.getBitmap9Patch();
+        }
+
+    }
+}
---
+++ PhoneApp9Patch/PhoneApp9Patch/UI9Patch/UI/ListBox9Patch.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace PhoneApp9Patch.UI9Patch.UI
+{
+    public partial class ListBox9Patch : UserControl
+    {
+
+        private BitmapImage imageSource;
+
+        public ListBox9Patch()
+        {
+            InitializeComponent();
+        }
+
+        private String _source;
+        public String Source
+        {
+            get { return _source; }
+            set
+            {
+                _source = value;
+
+                imageSource = new BitmapImage(new Uri(_source, UriKind.Relative));
+                imageSource.CreateOptions = BitmapCreateOptions.None;
+                imageSource.ImageFailed += new EventHandler<ExceptionRoutedEventArgs>(bi_ImageFailed);
+                imageSource.ImageOpened += new EventHandler<RoutedEventArgs>(bi_ImageOpened);
+            }
+        }
+
+        void bi_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            throw e.ErrorException;
+        }
+
+        void bi_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            NinePatch mNinePath = new NinePatch((BitmapImage)sender, (int)this.ActualWidth, (int)this.ActualHeight);
+            ImageBrush mBruch = new ImageBrush();
+            mBruch.ImageSource = mNinePath.getBitmap9Patch();
+            ListBox.Background = mBruch;
+            ListBox.Padding = mNinePath.getPadding();
+        }
+    }
+}
